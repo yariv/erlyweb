bf57410797eadd16c8764826709dd3dd7b9ae26e
yarivvv <yarivvv>
2007-10-31 05:08:28 +0000

    changed reverse_alias to fk_base

--- src/erlydb/erlydb.erl
+++ src/erlydb/erlydb.erl
@@ -919,46 +919,40 @@ make_some_to_many_forms(MetaMod, OtherModule, Alias, ExtraCurryParams,
 
 
 %% Get the relation name and primary/foreign key field mappings for
-%% a give relation.
+%% a give one-to-many or many-to-one relation.
 get_rel_options(Module, OtherModule, TablesData, ReverseFieldOrder) ->
     Res = {OtherMod, _Alias, PkFks} =
 	case OtherModule of
 	    OtherMod1 when is_atom(OtherMod1) ->
 		{OtherMod1, OtherMod1,
 		 pk_fk_fields2(OtherMod1, OtherMod1, TablesData)};
-	    {OtherMod2, Opts} ->
+	    {OtherMod1, Opts} ->
 		Alias1 =
 		    case proplists:get_value(alias, Opts) of
 			undefined ->
-			    OtherMod2;
+			    OtherMod1;
 			Other ->
 			    Other
 		    end,
-		Alias2 =
-		    if not ReverseFieldOrder ->
-			    case proplists:get_value(
-				   reverse_alias, Opts) of
-				undefined ->
-				    OtherMod2;
-				RevAlias ->
-				    RevAlias
-			    end;
-		       true ->
-			    Alias1
+		FkBase =
+		    case proplists:get_value(fk_base, Opts) of
+			undefined ->
+			    OtherMod1;
+			RevAlias ->
+			    RevAlias
 		    end,
 		PkFks1 = case proplists:get_value(foreign_keys, Opts) of
 			    undefined ->
-				pk_fk_fields2(OtherMod2,
-					      Alias2, TablesData);
+				pk_fk_fields2(OtherMod1,
+					      FkBase, TablesData);
 			    Other1 ->
 				Other1
 			end,
-		{OtherMod2, Alias1, PkFks1}
+		{OtherMod1, Alias1, PkFks1}
 	end,
     verify_field_mappings(Module, OtherMod,
 			  TablesData, PkFks, ReverseFieldOrder),
     Res.
-
 
 %% Verify all mapped fields are present.
 %%
